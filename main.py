84e57da688e834fc4d63d7c1406be03bd3561b72
lukasz0603 <84631919+lukasz0603@users.noreply.github.com>
2025-07-14 22:22:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Form, UploadFile, File, HTTPException, Query
+from fastapi import FastAPI, Form, UploadFile, File, HTTPException, Query, Body
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse,JSONResponse
 import requests
